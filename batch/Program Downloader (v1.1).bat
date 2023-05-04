7a707b053c570cb1872033ba8537e1e32dd5e3f0
MichalCZ123 <misa.prorok10@gmail.com>
2023-05-04 14:42:20 +0200

    Program Downloader (v1.1)
    Program Downloader (v1.1)

--- batch/Program Downloader (v1.1).bat
+++ batch/Program Downloader (v1.1).bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCmDJEmW+0UiKRZHcDSQM2yoB7hczOfs4eaIo0kYRt42e4Dn07eBbukQ5SU=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCmDJEmW+0UiKRZHcDSQM2yoB7hczOfs4eaIo0kYRt46YIrn6qaBJ/IS5wXAeoEj2nVbnM4eTFNOL1z6J0ExsWsi
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 title Program Downloader (v1.1)
 
